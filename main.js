50cc813a809c8873bd8e937480a018abf491a572
cwng <jkmilop@yahoo.com>
2021-07-15 12:15:05 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const strains = [];
+
+strains.push("ak-47");
+strains.unshift("marianoso");
+strains[2] = "regular";
+
+for(let i=0;i<strains.length;i++){
+    console.log(strains[]);
+}
+
+
+//for each
+for(let name of strains){
+    console.log(name);
+}

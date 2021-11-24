8627976db554d4e6df698fdc36385bf556eb253f
Juan Camilo Peña <jkmilop@yahoo.com>
2021-11-24 11:06:47 -0500

    Mas amigable

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const strains = [];
+let palindrome = str => {
+    str.replace(/\W/g, '')
+    str.toUpperCase()
+    let s = str.length
+    let ar
+    let m = Math.ceil(s / 2)
+    if (s % 2 === 0) ar = str.slice(0, m)
+    else ar = str.slice(0, m - 1)
+    let li = str.slice(m)
+    let er = li.split("").reverse().join("");
 
-strains.push("ak-47");
-strains.unshift("marianoso");
-strains[2] = "regular";
+    return (ar == er)
 
-for(let i=0;i<strains.length;i++){
-    console.log(strains[]);
 }
 
-
-//for each
-for(let name of strains){
-    console.log(name);
-}
+palindrome("eye")
